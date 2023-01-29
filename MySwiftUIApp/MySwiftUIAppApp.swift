3cf6b05512d423eedfb17882dca69b84516b1db2
donskikhmaksim <donskikh.maksim@gmail.com>
2023-01-29 20:42:08 +0400

    Initial Commit

---
+++ MySwiftUIApp/MySwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIAppApp.swift
+//  MySwiftUIApp
+//
+//  Created by Максим Донских on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
